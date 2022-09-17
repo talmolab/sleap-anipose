ce7f273b0e0d17f53931b33f7c0866d0a2143819
Sean Afshar <seanafshar97@gmail.com>
2022-09-16 18:10:21 -0700

    removed commented out reproject function from high level imports

--- sleap_anipose/__init__.py
+++ sleap_anipose/__init__.py
@@ -1,3 +1,3 @@
 """High-level imports."""
 from sleap_anipose.calibration import calibrate, draw_board
-from sleap_anipose.triangulation import load_view, load_tracks, triangulate, reproject
+from sleap_anipose.triangulation import load_view, load_tracks, triangulate
